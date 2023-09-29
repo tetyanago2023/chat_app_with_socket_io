5e4e169693b199d50f6117c6bd11aac48333a1f2
go2023 <tetyanago2018@gmail.com>
2023-09-29 16:33:14 -0700

    Express framework was added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening at port${PORT}`)
+});
+
+
+const io = new Server(expressServer, {
     cors: {
         // origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
 
@@ -20,5 +34,3 @@ io.on('connection', socket => {
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 });
-
-httpServer.listen(3500, () => console.log('listening on port 3500'));
